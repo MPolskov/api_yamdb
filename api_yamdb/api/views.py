d0565acf5cad6ba613b3e9e84912c8006ae903f8
Михаил Полшков <m@polshkov.ru>
2023-06-14 23:10:42 +0500

    Добавил чуть-чуть авторизации

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -34,7 +34,8 @@ class GenreViewSet(CustomViewSet):
 
 class TitleViewSet(ModelViewSet):
     queryset = Title.objects.all()
-    permission_classes = (IsAdministrator | IsAuthenticatedOrReadOnly, )
+    # permission_classes = (IsAdministrator | IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAdministrator, )
 
     def get_serializer_class(self):
         if self.action == 'list' or self.action == 'retrieve':
