38a321244a5887dc5f24249eb9dbb966d737f5cb
JohnnyFes <johnny@list.ru>
2023-06-17 16:18:34 +0500

    фикс: добавил IsAdministratorOrReadOnly

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,16 +1,23 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+def is_role(request, role):
+    if request.user and request.user.is_authenticated:
+        return request.user.role == role
+    return False
 
 
 class IsAdministrator(BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return (request.user.role == 'admin'
-                    or request.user.is_superuser)
-        return False
+        return is_role(request, 'admin')
 
 
 class IsModerator(BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return request.user.role == 'moderator'
-        return False
+        return is_role(request, 'moderator')
+
+
+class IsAdministratorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS
+                or is_role(request, 'admin'))
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,10 +1,10 @@
 from rest_framework import mixins
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 from reviews.models import Category, Genre, Title
-from .permissions import IsAdministrator
+from .permissions import IsAdministratorOrReadOnly
 from .serializers import (
     CategorySerializer,
     GenreSerializer,
@@ -19,7 +19,7 @@ class CustomViewSet(GenericViewSet, mixins.ListModelMixin,
     lookup_field = 'slug'
     filter_backends = (SearchFilter,)
     search_fields = ('name',)
-    permission_classes = (IsAdministrator | IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAdministratorOrReadOnly, )
 
 
 class CategoryViewSet(CustomViewSet):
@@ -34,7 +34,9 @@ class GenreViewSet(CustomViewSet):
 
 class TitleViewSet(ModelViewSet):
     queryset = Title.objects.all()
-    permission_classes = (IsAdministrator | IsAuthenticatedOrReadOnly, )
+    filter_backends = (DjangoFilterBackend, )
+    filterset_fields = ('category', 'genre', 'name', 'year')
+    permission_classes = (IsAdministratorOrReadOnly, )
 
     def get_serializer_class(self):
         if self.action == 'list' or self.action == 'retrieve':
