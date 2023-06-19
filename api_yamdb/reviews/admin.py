bc89db44d3b5fce6bdb1abd09ca80a90f2030095
Айрат Минниев <miniam@yandex.ru>
2023-06-19 23:04:39 +0500

    добавлен comment

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Genre, Title, GenreTitle, Review
+from .models import Category, Genre, Title, GenreTitle, Review, Comment
 
 
 admin.site.register(Category)
@@ -8,3 +8,4 @@ admin.site.register(Genre)
 admin.site.register(Title)
 admin.site.register(GenreTitle)
 admin.site.register(Review)
+admin.site.register(Comment)
