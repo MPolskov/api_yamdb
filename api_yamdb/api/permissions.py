3cf3c56bde1a6dedf0ef392ec4c5d46b41fe8343
Михаил Полшков <m@polshkov.ru>
2023-06-25 10:35:06 +0500

    fix permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -31,7 +31,7 @@ class IsAuthorModeratorAdminOrReadOnly(BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        return (True if request.method in SAFE_METHODS
-                else (obj.author == request.user
-                      or request.user.role in STAFF_ROLES
-                      or request.user.is_superuser))
+        return (request.method in SAFE_METHODS
+                or (obj.author == request.user
+                    or request.user.role in STAFF_ROLES
+                    or request.user.is_superuser))
